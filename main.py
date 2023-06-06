ada2900cfe5e6008436f656571e0f42d1b7a6af7
Peter Feliciano <peter.feliciano@canoeintelligence.com>
2023-06-05 20:40:06 -0400

    writing to fs and adding aes support

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import traceback
 import camelot
 import tempfile
 import pickle
-
+from werkzeug.utils import secure_filename
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -23,25 +24,14 @@ def go_camelot():
             temporary_named_file = tempfile.NamedTemporaryFile(suffix='.pdf')
             temporary_named_file.write(temporary_file.read())
             tables = camelot.read_pdf(temporary_named_file.name, flavor='stream', strict=False, pages='1-6')
-
-            for t in tables:
-                print(t.df, flush=True)
+            for idx, t in enumerate(tables):
+                print(t.df.to_string(), flush=True)
                 print(t.parsing_report, flush=True)
+                with open(f"{secure_filename(temporary_file.filename)}_{idx}.txt", "w") as f:
+                    f.write(t.df.to_string())
+                    f.write(str(t.parsing_report))
+
             temporary_named_file.close()
-        return "done!", 200 # jsonify(tables), 200 
-    except: 
-        ex = traceback.format_exc()
-        print(str(ex), flush=True)
-        return str(ex), 500
-
-@app.route('/camelottest', methods=['POST'])
-def go_extract_table():
-    try:
-        tables = camelot.read_pdf('hlane_1.pdf', flavor='stream', strict=False, pages='1-6')
-        for t in tables:
-            print(t.df, flush=True)
-            print(t.parsing_report, flush=True)
-
         return "done!", 200 # jsonify(tables), 200 
     except: 
         ex = traceback.format_exc()
