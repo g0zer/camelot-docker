68aff06ecd0ed94698e49df259626d3e7e7209a0
Peter Feliciano <peter.feliciano@canoeintelligence.com>
2023-07-27 09:32:54 -0400

    adding json response

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ app = Flask(__name__)
 @app.route('/extract', methods=['POST'])
 def go_camelot():
     try:
+        response = {}
         temporary_file = request.files['file']
         if temporary_file:
             temporary_named_file = tempfile.NamedTemporaryFile(suffix='.pdf')
             temporary_named_file.write(temporary_file.read())
-            tables = camelot.read_pdf(temporary_named_file.name, flavor='stream', strict=False, pages='1-6')
+            tables = camelot.read_pdf(temporary_named_file.name, flavor='stream', strict=False, pages='all')
             for idx, t in enumerate(tables):
                 print(t.df.to_string(), flush=True)
                 print(t.parsing_report, flush=True)
-                with open(f"{secure_filename(temporary_file.filename)}_{idx}.txt", "w") as f:
-                    f.write(t.df.to_string())
-                    f.write(str(t.parsing_report))
+                tbl_result = {"table": t.df.to_dict(), "page": t.page, "order": t.order, "parsing_report": t.parsing_report}
+                response[f"{idx}"] = tbl_result
 
             temporary_named_file.close()
-        return "done!", 200 # jsonify(tables), 200 
+        return response, 200 # , 200 
     except: 
         ex = traceback.format_exc()
         print(str(ex), flush=True)
